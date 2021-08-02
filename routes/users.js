5aaf8cd9db5db6ddd757dc28e1af7e31f36a7cc5
Palfalvi Lorand <palfalvi.lorand@gmail.com>
2021-08-02 18:44:34 +0300

    add error handling

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,23 @@ const express = require("express");
 const router = express.Router();
 const { firestoreDB: firestore } = require("../db/firestoreDB");
 
+// TODO: Move outside
+class HttpError extends Error {
+  constructor(statusCode, message) {
+    super();
+    this.name = "HttpError";
+    this.message = message;
+    this.statusCode = statusCode ?? 500;
+  }
+}
+
+// TODO: Move outside
+function errorHandler({ res, error }) {
+  return res.status(error.statusCode).json({
+    message: error.message,
+  });
+}
+
 function getDocumentReference(doc) {
   return firestore.collection("users").doc(doc);
 }
@@ -35,39 +52,37 @@ async function quickstart() {
 /* GET users listing. */
 // Using route parameters /:user_id
 router.get("/users/:user_id/profile", async function (req, res) {
-  // 1. Validate input
-  const userID = req.params.user_id;
-  if (!userID) {
-    return res.status(400).json({
-      message: "userID query parameter should be defined",
-    });
-  }
-  // 2. Perform the logic of your endpoint
-  const document = getDocumentReference(userID);
-  const doc = await document.get();
-  const data = doc.data();
+  try {
+    // 1. Validate input
+    const userID = req.params.user_id;
+    if (!userID) {
+      throw new HttpError(400, "userID query parameter should be defined");
+    }
+    // 2. Perform the logic of your endpoint
+    const document = getDocumentReference(userID);
+    const doc = await document.get();
+    const data = doc.data();
 
-  // 3. Handle errors
-  if (!data) {
-    return res.status(404).json({
-      message: "User not found",
-    });
-  }
+    // 3. Handle errors
+    if (!data) {
+      throw new HttpError(404, "User not found");
+    }
 
-  if (!data?.profile) {
-    return res.status(404).json({
-      message: "Internal data schema is wrong",
-    });
-  }
+    if (!data?.profile) {
+      throw new HttpError(500, "Internal data schema is wrong");
+    }
 
-  // 4. Return response
-  return res.status(200).json(data.profile);
+    // 4. Return response
+    return res.status(200).json(data.profile);
+  } catch (error) {
+    return errorHandler({ res, error });
+  }
 });
 
 router.put("/users/:user_id/profile", async function (req, res) {
   const userID = req.params.user_id;
+  console.log(req.body);
   const document = getDocumentReference(userID);
-  console.log(req);
   const doc = await document.update({
     profile: {
       gender: "prefer not to say",
