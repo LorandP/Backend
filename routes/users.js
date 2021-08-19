f718c819c0bd51369f035be4185d19759188dfce
Palfalvi Lorand <palfalvi.lorand@gmail.com>
2021-08-19 22:55:57 +0300

    add post method

--- routes/users.js
+++ routes/users.js
@@ -36,7 +36,7 @@ async function quickstart() {
 
 /* GET users listing. */
 // Using route parameters /:user_id
-router.get("/users/:user_id/profile", async function (req, res) {
+router.get("/users/:user_id", async function (req, res) {
   try {
     // 1. Validate input
     const userID = req.params.user_id;
@@ -53,18 +53,14 @@ router.get("/users/:user_id/profile", async function (req, res) {
       throw new HttpError(404, "User not found");
     }
 
-    if (!data?.profile) {
-      throw new HttpError(500, "Internal data schema is wrong");
-    }
-
     // 4. Return response
-    return res.status(200).json(data.profile);
+    return res.status(200).json(data);
   } catch (error) {
     return errorHandler({ res, error });
   }
 });
 
-router.put("/users/:user_id/profile", async function (req, res) {
+router.put("/users/:user_id", async function (req, res) {
   try {
     // Verify input
     const userID = req.params.user_id;
@@ -72,16 +68,24 @@ router.put("/users/:user_id/profile", async function (req, res) {
       throw new HttpError(400, "userID query parameter should be defined");
     }
     const gender = req.body?.gender;
-    if (!gender) {
-      throw new HttpError(400, "gender should be defined");
+    const birthday = req.body?.birthday;
+    let dataToUpdate = {};
+    if (gender) {
+      dataToUpdate = {
+        gender,
+      };
+    }
+    if (birthday) {
+      dataToUpdate = {
+        ...dataToUpdate,
+        birthday,
+      };
     }
 
     // Perform logic
     const document = getDocumentReference(userID);
     await document.update({
-      profile: {
-        gender,
-      },
+      ...dataToUpdate,
     });
 
     return res.status(200).json({
@@ -94,7 +98,29 @@ router.put("/users/:user_id/profile", async function (req, res) {
 
 router.post("/users", async function (req, res) {
   try {
-    
+    // Verify input
+    const userID = req.body?.userID;
+    if (!userID) {
+      throw new HttpError(400, "userID property should be defined");
+    }
+    const gender = req.body?.gender;
+    if (!gender) {
+      throw new HttpError(400, "gender property should be defined");
+    }
+    const birthday = req.body?.birthday;
+    if (!birthday) {
+      throw new HttpError(400, "birthday property should be defined");
+    }
+    // Perform logic
+    const data = {
+      gender,
+      birthday,
+    };
+    const result = await firestore.collection("users").doc(userID).set(data);
+
+    return res.status(200).json({
+      title: "Post successful!",
+    });
   } catch (error) {
     return errorHandler({ res, error });
   }
